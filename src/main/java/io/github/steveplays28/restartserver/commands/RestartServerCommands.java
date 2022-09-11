039b148ec4947f5f8215bdf09dabfcc0af0e9ad9
Darion Spaargaren <djspaargaren@outlook.com>
2022-09-11 23:42:37 +0200

    Initial commit

---
+++ src/main/java/io/github/steveplays28/restartserver/commands/RestartServerCommands.java
@@ -0,0 +1,11 @@
+package io.github.steveplays28.restartserver.commands;
+
+import net.minecraft.text.Text;
+
+import static net.minecraft.server.command.CommandManager.*;
+
+public class RestartServerCommands {
+	public void initialize() {
+
+	}
+}
