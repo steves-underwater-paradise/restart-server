92d2154981e8c3f328e444b720260d38b6e0485d
Darion Spaargaren <djspaargaren@outlook.com>
2022-09-12 18:26:24 +0200

    Add proper color formatting to text

--- src/main/java/io/github/steveplays28/restartserver/commands/RestartCommand.java
+++ src/main/java/io/github/steveplays28/restartserver/commands/RestartCommand.java
@@ -5,30 +5,49 @@ import io.github.steveplays28.restartserver.RestartServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.apache.commons.lang3.SystemUtils;
 
 import java.io.IOException;
 
 public class RestartCommand {
-	public static String name = "restart";
-	public static int permissionLevel = 4;
+	public static final String NAME = "restart";
+	public static final int PERMISSION_LEVEL = 4;
+
+	public static String restartScript;
 
 	public static LiteralArgumentBuilder<ServerCommandSource> register() {
-		return CommandManager.literal(name).executes(ctx -> execute(ctx.getSource())).requires((ctx) -> ctx.hasPermissionLevel(permissionLevel));
+		return CommandManager.literal(NAME).executes(ctx -> execute(ctx.getSource())).requires((ctx) -> ctx.hasPermissionLevel(PERMISSION_LEVEL));
 	}
 
 	private static int execute(ServerCommandSource source) {
-		source.getServer().getPlayerManager().broadcast(Text.of(String.format("§eRestarting server... (eta: %s)", RestartServer.config.eta)), false);
+		// Send restart message to console and all players
+		source.getServer().getPlayerManager().broadcast(Text.literal(String.format("[RestartServer] %s", RestartServer.config.restartMessage)).formatted(Formatting.YELLOW), false);
 
 		try {
-			ProcessBuilder process = new ProcessBuilder(RestartServer.config.restartFile);
+			// Determine OS
+			if (SystemUtils.IS_OS_LINUX) {
+				restartScript = RestartServer.config.restartScriptLinux;
+			} else if (SystemUtils.IS_OS_MAC || SystemUtils.IS_OS_MAC_OSX) {
+				restartScript = RestartServer.config.restartScriptMacOS;
+			} else if (SystemUtils.IS_OS_WINDOWS) {
+				restartScript = RestartServer.config.restartScriptWindows;
+			}
+
+			// Start server restart script process
+			ProcessBuilder process = new ProcessBuilder(restartScript);
 			process.start();
-			source.getServer().sendMessage(Text.of(String.format("[RestartServer] Restarting server using %s...", RestartServer.config.restartFile)));
+			source.getServer().sendMessage(Text.literal(String.format("[RestartServer] Restarting server using script %s...", restartScript)).formatted(Formatting.YELLOW));
 		} catch (IOException e) {
 			e.printStackTrace();
+
+			// Send restart failed message to console and all players
+			source.getServer().getPlayerManager().broadcast(Text.literal("[RestartServer] Server restart failed.").formatted(Formatting.RED), false);
 		}
 
+		// Stop server
 		source.getServer().stop(false);
-		source.getServer().sendMessage(Text.of("[RestartServer] Stopping server..."));
+
 		return 1;
 	}
 }
--- src/main/java/io/github/steveplays28/restartserver/config/RestartServerConfig.java
+++ src/main/java/io/github/steveplays28/restartserver/config/RestartServerConfig.java
@@ -3,9 +3,15 @@ package io.github.steveplays28.restartserver.config;
 import io.github.steveplays28.restartserver.RestartServer;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = RestartServer.MOD_ID)
 public class RestartServerConfig implements ConfigData {
-	public String restartFile = "start-server.bat";
-	public String eta = "2 minutes";
+	@ConfigEntry.Category("Restart script")
+	public String restartScriptLinux = "start-server.sh";
+	public String restartScriptMacOS = "start-server.sh";
+	public String restartScriptWindows = "start-server.bat";
+
+	@ConfigEntry.Category("Restart message")
+	public String restartMessage = "Restarting server... (eta: ~2m)";
 }
